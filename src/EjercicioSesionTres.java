339631fe88cf5134dc5b3fdfe6ffa16c8a899a68
tony_ <antonyrebolledo@gmail.com>
2021-11-24 11:08:23 -0600

    Ejercicio Resuelto Sesion 3

---
+++ src/EjercicioSesionTres.java
@@ -0,0 +1,11 @@
+public class EjercicioSesionTres {
+
+    public static void main(String[] args) {
+
+        String[] diasDeLaSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes","Sabado", "Domingo"};
+
+        for (String diasdelasemana : diasDeLaSemana){
+            System.out.println("Estos son los dias de la semana: "+ diasdelasemana);
+        }
+    }
+}
