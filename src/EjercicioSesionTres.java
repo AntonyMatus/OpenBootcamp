0c7c0842be65836752b1d0c5bf85816df6ce43d2
tony_ <antonyrebolledo@gmail.com>
2021-11-24 11:30:20 -0600

    Correccion Ejercicio 3

--- src/EjercicioSesionTres.java
+++ src/EjercicioSesionTres.java
@@ -4,8 +4,12 @@ public class EjercicioSesionTres {
 
         String[] diasDeLaSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes","Sabado", "Domingo"};
 
-        for (String diasdelasemana : diasDeLaSemana){
-            System.out.println("Estos son los dias de la semana: "+ diasdelasemana);
+        String cadenaConcatenada = "";
+
+        for (int i = 0; i < diasDeLaSemana.length; i++){
+            cadenaConcatenada = cadenaConcatenada + " " + diasDeLaSemana[i];
         }
+
+        System.out.println(cadenaConcatenada);
     }
 }
